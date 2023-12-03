caba5525b53f10019610af406fedbe605d4cc452
gkraljic <L00162123@atu.ie>
2023-12-03 18:09:55 +0000

    next commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function handleEditProfile() {
+    document.getElementById("edit-view").style.display = "block"
+    document.getElementById("display-view").style.display = "none"
+}
+
+function handleUpdateProfile() {
+    document.getElementById("edit-view").style.display = "none"
+    document.getElementById("display-view").style.display = "block"
+}  
